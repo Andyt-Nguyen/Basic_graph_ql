39c0585b31a6474982b0ef9d4d2597eabf290f8b
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2019-11-23 06:16:10 -0700

    update mutations

--- schema/schema.js
+++ schema/schema.js
@@ -98,6 +98,21 @@ const RootQuery = new GraphQLObjectType({
          const res = await axios.delete(`http://localhost:3000/users/${args.id}`);
          return res.data;
        }
+     },
+
+     editUser: {
+       type: UserType,
+       args: {
+         id: { type: new GraphQLNonNull(GraphQLString) },
+         firstName: { type: GraphQLString },
+         age: { type: GraphQLInt },
+         companyId: { type: GraphQLString }
+       },
+       async resolve(parentValue, args) {
+         const res = await axios.patch(`http://localhost:3000/users/${args.id}`, args)
+
+         return res.data;
+       }
      }
    }
  })
