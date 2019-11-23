6be2cc7c1571ea2cab83f1b1f5544c7d0a7ce011
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2019-11-23 04:14:15 -0700

    make http request

--- schema/schema.js
+++ schema/schema.js
@@ -1,17 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const { 
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema
 } = graphql;
-
-
-const users = [
-  { id: '25', firstName: 'bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 25 },
-]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,8 +22,9 @@ const RootQuery = new GraphQLObjectType({
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
-      resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+      async resolve(parentValue, args) {
+        const res = await axios.get(`http://localhost:3000/users/${args.id}`);
+        return res.data;
       }
     }
   }
